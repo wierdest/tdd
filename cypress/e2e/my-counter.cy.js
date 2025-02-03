13bbe3c64cbb637c858b855329ec247e2b3de66f
andreluiz.dsn.pack <andreluiz.dsn.pack@alterdata.com.br>
2025-02-03 07:14:02 -0300

    more tests in tdd

--- cypress/e2e/my-counter.cy.js
+++ cypress/e2e/my-counter.cy.js
@@ -1,27 +1,55 @@
 describe('MyCounter Component', () => {
-
   beforeEach(() => { 
     cy.visit('/')
   })
-
+  
   it('deve existir', () => {
-    cy.get('my-counter').then(el => el[0].shadowRoot).should('exist')
+   cy.get('my-counter').then($el => $el[0].shadowRoot).should('exist')
   })
 
+  it('deve conter um título', () => {
+    cy.get('my-counter').then($el => $el[0].shadowRoot).find('h1').should('exist')
+  })
+
+  it('deve conter texto MyCounter no título', () => {
+    cy.get('my-counter').then($el => $el[0].shadowRoot).find('h1').should('contain', 'MyCounter')
+  })
+  
   it('deve conter um botão', () => {
-    cy.get('my-counter').then(el => el[0].shadowRoot).find('button').should('exist')
+    cy.get('my-counter').then($el => $el[0].shadowRoot).find('button').should('exist')
   })
 
-  it('deve conter o texto incrementar no botão', () => {
-    cy.get('my-counter').then(el => el[0].shadowRoot).find('button').should('contain', 'Incrementar')
+  it('deve conter o texto Incrementar até o máximo de 2 no botão', () => {
+    cy.get('my-counter').then($el => $el[0].shadowRoot).find('button').should('contain', 'Incrementar até o máximo de 2')
   })
 
   it('deve conter um span', () => {
-    cy.get('my-counter').then(el => el[0].shadowRoot).find('span').should('exist')
+    cy.get('my-counter').then($el => $el[0].shadowRoot).find('span').should('exist')
   })
   
   it('deve mostrar valor inicial 1', () => {
-    cy.get('my-counter').then(el => el[0].shadowRoot).find('span').should('contain', 1)
+    cy.get('my-counter').then($el => $el[0].shadowRoot).find('span').should('contain', 1)
   })
 
+  it('deve incrementar o valor no click do botão', () => {
+    cy.get('my-counter').then($el => $el[0].shadowRoot).find('button').click()
+    cy.get('my-counter').then($el => $el[0].shadowRoot).find('span').should('contain', 2)
+  })
+
+  it('deve limitar o valor a 2, retornando o valor a 1 ao ultrapassar 2', () => {
+    cy.get('my-counter').then($el => $el[0].shadowRoot).find('button').click().click()
+    cy.get('my-counter').then($el => $el[0].shadowRoot).find('span').should('contain', 1)
+  })
+
+  it('deve conter um span acima do checkbox', () => {
+    cy.get('my-counter').then($el => $el[0].shadowRoot).find('span:contains(Checkbox)')
+  })
+
+  it('deve conter um checkbox', () => {
+    cy.get('my-counter').then($el => $el[0].shadowRoot).find('[type="checkbox"]').should('exist')
+  })
+
+  it('deve digitar no input', () => {
+    cy.get('my-counter').then($el => $el[0].shadowRoot).find('[type="text"]').type('Vão bora!')
+  })
 })
--- public/src/my-counter.js
+++ public/src/my-counter.js
@@ -1,24 +1,62 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 
 export class MyCounter extends LitElement {
   static properties = {
     count: { type: Number },
-  };
+  }
+
+  static styles = [
+    css`
+      .page {
+        display: flex;
+        gap: 20px;
+        flex-direction: column
+      }
+    `
+  ]
+  
+  set count (value) {
+    this._count = value
+    // aqui pode ter uma lógica para atualizar 
+    // outros valores quando Count mudar, por exemplo
+  }
+
+  get count () {
+    // é preciso retornar uma variável diferente para evitar um loop infinito
+    return this._count
+  }
+
+  _increment() {
+    this.count = this.count + 1
+    if (this.count > 2) {
+      this.count = 1
+    }
+    this.requestUpdate()
+  }
 
   constructor() {
     super();
-    this.count = 1;
+    this.count = 1
   }
-
 
   render() {
     return html`
-      <div>
-        <span>${this.count}</span>
-        <button>Incrementar</button>
+      <div class="page">
+        <h1>MyCounter</h1>
+        <div>
+          <span>${this.count}</span>
+          <button @click=${() => this._increment()}>Incrementar até o máximo de 2</button>
+        </div>
+        <div>
+          <span>Checkbox</span>
+          <input type="checkbox"/>
+        </div>
+        <div>
+          <input type="text" placeholder="Digite aqui"/>
+        </div>
       </div>
-    `;
+    `
   }
 }
 
-customElements.define('my-counter', MyCounter);
+customElements.define('my-counter', MyCounter)
